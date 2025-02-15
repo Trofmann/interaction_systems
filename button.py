00ee3d866136002f6bb02adae30235aa9a40de98
Evgeniy <trofimovevgeniy9@gmail.com>
2025-02-15 13:18:39 +0300

    Переопределён метод eq

--- button.py
+++ button.py
@@ -8,5 +8,13 @@ class DigitButton:
         self.value = value
         self.is_numpad = is_numpad
 
+    def __str__(self):
+        return f'{self.value} {self.is_numpad}'
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, DigitButton):
+            return self.value == other.value and self.is_numpad == other.is_numpad
+        return False
+
     def __hash__(self):
         return hash((self.value, self.is_numpad))
