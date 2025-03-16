f5f1a27b39564642e55d098bff40cec63f5a4807
Evgeniy <trofimovevgeniy9@gmail.com>
2025-03-16 10:51:05 +0300

    Описание меню

---
+++ lab3/menu.py
@@ -0,0 +1,193 @@
+from collections import OrderedDict
+from typing import Self
+
+__all__ = [
+    'MenuItem',
+    'menu_description',
+]
+
+
+class MenuItem:
+    """Элемент меню"""
+
+    def __init__(self, label: str, code: str, children: list[Self] | None = None):
+        self.label = label
+        self.code = code
+        self.parent: Self | None = None
+        self.children: list[Self] = children if children is not None else []
+        self._set_children_parent()
+
+    def __str__(self):
+        return self.full_path
+
+    def _set_children_parent(self):
+        """Установка дочерним элементам родительского"""
+        for c in self.children:
+            c.parent = self
+
+    @property
+    def full_code(self) -> str:
+        if self.parent:
+            return f'{self.parent.full_code}'
+        return self.code
+
+    @property
+    def full_path(self) -> str:
+        if self.parent:
+            return f'{self.parent.full_path}->{self.label}'
+        return self.label
+
+
+menubar_description: OrderedDict[str, MenuItem] = OrderedDict()
+menubar_description['file'] = MenuItem(
+    label='File',
+    code='file',
+    children=[
+        MenuItem(
+            label='Open',
+            code='open',
+        ),
+        MenuItem(
+            label='Save',
+            code='save',
+        ),
+        MenuItem(
+            label='New',
+            code='new',
+        ),
+        MenuItem(
+            label='Close',
+            code='close',
+        ),
+        MenuItem(
+            label='Export',
+            code='export',
+            children=[
+                MenuItem(
+                    label='XLSX',
+                    code='xlsx',
+                ),
+                MenuItem(
+                    label='XLS',
+                    code='xls',
+                ),
+                MenuItem(
+                    label='CSV',
+                    code='csv',
+                    children=[
+                        MenuItem(
+                            label='separator ;',
+                            code='separator_semicolon',
+                        ),
+                        MenuItem(
+                            label='separator ,',
+                            code='separator_comma',
+                        )
+                    ],
+                )
+            ],
+        )
+    ],
+)
+menubar_description['edit'] = MenuItem(
+    label='Edit',
+    code='edit',
+    children=[
+        MenuItem(
+            label='Cut',
+            code='cut',
+        ),
+        MenuItem(
+            label='Copy',
+            code='copy',
+        ),
+        MenuItem(
+            label='Paste',
+            code='paste',
+        ),
+        MenuItem(
+            label='Delete',
+            code='delete',
+        )
+    ],
+)
+menubar_description['view'] = MenuItem(
+    label='View',
+    code='view',
+    children=[
+        MenuItem(
+            label='Ruler',
+            code='ruler',
+        ),
+        MenuItem(
+            label='Grid',
+            code='grid',
+        ),
+        MenuItem(
+            label='Tools',
+            code='tools',
+            children=[],
+        ),
+        MenuItem(
+            label='Font',
+            code='font',
+            children=[
+                MenuItem(
+                    label='Size',
+                    code='size',
+                    children=[
+                        MenuItem(
+                            label='Increase',
+                            code='increase_font',
+                        ),
+                        MenuItem(
+                            label='Decrease',
+                            code='decrease_font',
+                        )
+                    ]
+                )
+            ]
+        ),
+        MenuItem(
+            label='Color',
+            code='color',
+        ),
+        MenuItem(
+            label='Scale',
+            code='scale',
+            children=[
+                MenuItem(
+                    label='Increase',
+                    code='increase_scale',
+                ),
+                MenuItem(
+                    label='Decrease',
+                    code='decrease_scale',
+                )
+            ]
+        )
+    ],
+)
+menubar_description['window'] = MenuItem(
+    label='Window',
+    code='window',
+    children=[],
+)
+menubar_description['help'] = MenuItem(
+    label='Help',
+    code='help',
+    children=[
+        MenuItem(
+            label='Registration',
+            code='registration',
+        ),
+        MenuItem(
+            label='About',
+            code='about',
+        ),
+        MenuItem(
+            label='Payment',
+            code='payment',
+        )
+    ],
+)
