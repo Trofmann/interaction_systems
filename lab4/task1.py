0aa29c12d1f509a1a86dd94a912e5680a3af93a9
Evgeniy <trofimovevgeniy9@gmail.com>
2025-03-25 19:05:34 +0300

    Интерфейс

---
+++ lab4/task1.py
@@ -0,0 +1,45 @@
+import sys
+
+from PyQt5.QtWidgets import (
+    QMainWindow,
+    QApplication,
+    QTextEdit,
+    QLineEdit,
+)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self._setup_ui()
+        self._init_info_text_area()
+        self._init_search_input()
+
+    def _setup_ui(self):
+        self.setWindowTitle('Lab4')
+        self.setGeometry(500, 300, 1000, 700)
+
+    def _init_info_text_area(self):
+        self.info_text_area = QTextEdit(self)
+        self.info_text_area.setReadOnly(True)
+        self.info_text_area.move(500, 100)
+        self.info_text_area.setFixedWidth(400)
+        self.info_text_area.setFixedHeight(450)
+
+    def _init_search_input(self):
+        self.search_input = QLineEdit(self)
+        self.search_input.setPlaceholderText('Введите текст...')
+        self.search_input.move(100, 100)
+        self.search_input.setFixedWidth(300)
+        self.search_input.returnPressed.connect(self._on_search_input)
+
+    def _on_search_input(self):
+        text = self.search_input.text()
+        print(text)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
