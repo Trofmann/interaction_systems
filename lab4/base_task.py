799ce594dcf77e5bfc9db385fb84ef38823b09ff
Evgeniy <trofimovevgeniy9@gmail.com>
2025-03-27 22:06:05 +0300

    Вынес в базовый класс

---
+++ lab4/base_task.py
@@ -0,0 +1,39 @@
+from PyQt5.QtWidgets import (
+    QMainWindow,
+    QTextEdit,
+    QLineEdit,
+)
+
+from const import TEXT
+from logic import process_search
+
+
+class BaseTaskWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self._setup_ui()
+        self._init_info_text_area()
+        self._init_search_input()
+
+    def _setup_ui(self):
+        self.setWindowTitle('Lab4')
+        self.setGeometry(500, 300, 1000, 700)
+
+    def _init_info_text_area(self):
+        self.info_text_area = QTextEdit(self)
+        self.info_text_area.setReadOnly(True)
+        self.info_text_area.move(500, 100)
+        self.info_text_area.setFixedWidth(400)
+        self.info_text_area.setFixedHeight(450)
+        self.info_text_area.setText(TEXT)
+
+    def _init_search_input(self):
+        self.search_input = QLineEdit(self)
+        self.search_input.setPlaceholderText('Введите текст...')
+        self.search_input.move(100, 100)
+        self.search_input.setFixedWidth(300)
+        self.search_input.returnPressed.connect(self._on_search_input)
+
+    def _on_search_input(self):
+        term = self.search_input.text()
+        self.info_text_area.setText(process_search(term))
--- lab4/task2.py
+++ lab4/task2.py
@@ -2,46 +2,24 @@ import sys
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-    QMainWindow,
     QApplication,
-    QTextEdit,
-    QLineEdit, QCompleter,
+    QCompleter,
 )
 
-from const import TEXT, WORDS
+from base_task import BaseTaskWindow
+from const import WORDS
 from logic import process_search
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self._setup_ui()
-        self._init_info_text_area()
-        self._init_search_input()
-
-    def _setup_ui(self):
-        self.setWindowTitle('Lab4')
-        self.setGeometry(500, 300, 1000, 700)
-
-    def _init_info_text_area(self):
-        self.info_text_area = QTextEdit(self)
-        self.info_text_area.setReadOnly(True)
-        self.info_text_area.move(500, 100)
-        self.info_text_area.setFixedWidth(400)
-        self.info_text_area.setFixedHeight(450)
-        self.info_text_area.setText(TEXT)
+class Task2Window(BaseTaskWindow):
 
     def _init_search_input(self):
-        self.search_input = QLineEdit(self)
-        self.search_input.setPlaceholderText('Введите текст...')
-        self.search_input.move(100, 100)
-        self.search_input.setFixedWidth(300)
+        super()._init_search_input()
 
         self.completer = QCompleter(sorted(WORDS))
         self.completer.setFilterMode(Qt.MatchStartsWith)  # Фильтр по началу слова
         self.completer.setCaseSensitivity(Qt.CaseInsensitive)  # Без учёта регистра
         self.search_input.setCompleter(self.completer)
-        self.search_input.returnPressed.connect(self._on_search_input)
 
     def _on_search_input(self):
         term = self.search_input.text()
@@ -50,6 +28,6 @@ class MainWindow(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_window = MainWindow()
+    main_window = Task2Window()
     main_window.show()
     sys.exit(app.exec_())
