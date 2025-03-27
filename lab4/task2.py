72f8facac09cfd933211f8a7a8f8fbf7cbf96634
Evgeniy <trofimovevgeniy9@gmail.com>
2025-03-27 22:03:05 +0300

    С вариантами выбора

---
+++ lab4/task2.py
@@ -0,0 +1,55 @@
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QMainWindow,
+    QApplication,
+    QTextEdit,
+    QLineEdit, QCompleter,
+)
+
+from const import TEXT, WORDS
+from logic import process_search
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self._setup_ui()
+        self._init_info_text_area()
+        self._init_search_input()
+
+    def _setup_ui(self):
+        self.setWindowTitle('Lab4')
+        self.setGeometry(500, 300, 1000, 700)
+
+    def _init_info_text_area(self):
+        self.info_text_area = QTextEdit(self)
+        self.info_text_area.setReadOnly(True)
+        self.info_text_area.move(500, 100)
+        self.info_text_area.setFixedWidth(400)
+        self.info_text_area.setFixedHeight(450)
+        self.info_text_area.setText(TEXT)
+
+    def _init_search_input(self):
+        self.search_input = QLineEdit(self)
+        self.search_input.setPlaceholderText('Введите текст...')
+        self.search_input.move(100, 100)
+        self.search_input.setFixedWidth(300)
+
+        self.completer = QCompleter(sorted(WORDS))
+        self.completer.setFilterMode(Qt.MatchStartsWith)  # Фильтр по началу слова
+        self.completer.setCaseSensitivity(Qt.CaseInsensitive)  # Без учёта регистра
+        self.search_input.setCompleter(self.completer)
+        self.search_input.returnPressed.connect(self._on_search_input)
+
+    def _on_search_input(self):
+        term = self.search_input.text()
+        self.info_text_area.setText(process_search(term))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
